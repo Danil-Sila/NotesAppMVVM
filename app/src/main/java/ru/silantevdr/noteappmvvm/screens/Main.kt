134781016a6ed65f209a21327243819da944e2ea
Danil Silantev <silantev.danil@gmail.com>
2023-01-23 22:45:18 +0300

    lesson 1

---
+++ app/src/main/java/ru/silantevdr/noteappmvvm/screens/Main.kt
@@ -0,0 +1,8 @@
+package ru.silantevdr.noteappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Main(navController: NavHostController) {
+}
